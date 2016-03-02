26ac4865851634a9b419da9271f2d0f6fd2e112f
Guilherme Andrade <guilherme.andrade19@hotmail.com>
2016-03-01 22:39:09 -0300

    create structure of project

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
 	  loaders:[
 	  {test: /\.coffee$/, include: __dirname + '/app', loader: "coffee-loader"}	
 	  ]
+	},
+	
+	output:{
+	  filename: "index_bundle.js",
+	  path: __dirname + '/dist'
 	}
+
 }
